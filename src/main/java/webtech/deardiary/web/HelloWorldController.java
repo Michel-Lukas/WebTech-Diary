a2772e003a0055f62885edbe45260898bd920aa4
unknown <Lukas_Michel@outlook.com>
2021-11-25 14:01:53 +0100

    CI fixed

--- src/main/java/webtech/deardiary/web/HelloWorldController.java
+++ src/main/java/webtech/deardiary/web/HelloWorldController.java
@@ -1,2 +1,12 @@
-package webtech.deardiary.web;public class HelloWorldController {
+package webtech.deardiary.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping(path = "/")
+    public ModelAndView showHelloWorld() { return new ModelAndView("helloworld"); }
 }
